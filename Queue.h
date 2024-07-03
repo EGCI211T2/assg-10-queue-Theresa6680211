ee4e49d8cfd5437cd3f5db294f50320f370eb8a5
Theresa6680211 <167950940+Theresa6680211@users.noreply.github.com>
2024-07-03 04:14:43 +0000

    done

--- Queue.h
+++ Queue.h
@@ -17,19 +17,24 @@ void Queue::enqueue(int x){
   NodePtr new_node= new NODE(x);
 if(new_node){ 
   /* Add head and tail for me please */
-  //1. connect & Change tail
+  if(headPtr!=NULL) tailPtr->set_next(new_node); //if(size==0)
+
+  //2. (may be) change head  when the queue is empty
+  else headPtr=new_node;
+  
+  tailPtr=new_node;
+
+  /*
   if(headPtr!=NULL)
   {
     tailPtr->set_next(new_node);
     tailPtr=tailPtr->get_next();
   }
-
-  //2. (may be) change head  when the queue is empty
   else
   {
     headPtr=new_node;
     tailPtr=new_node;
-  }
+  }*/
   
   //3. increase size*/
   size++;
@@ -66,14 +71,12 @@ Queue::~Queue(){
     //delete all remaning Queue (i.e. DQ all) 
     cout<<"Clearing queue"<<endl;
     NodePtr t;
-    int value;
     for (int i=0; i<size; i++)
     {
         t=headPtr;
-        value=t->get_value();
         headPtr=headPtr->get_next();
 
-        cout<<"Dequeing "<<value<<endl;
+        cout<<"Dequeing "<<t->get_value()<<endl;
         delete t;
     }
     
